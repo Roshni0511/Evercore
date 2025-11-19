337151773b341296122e16b6de2238b288c20fac
Roshni0511 <roshnichauhan051@gmail.coom>
2025-11-19 11:38:22 +0530

    set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,41 @@ const Home = () => {
   return (
     <>
       <Navbar />
+
+        {/* <!-- Home Section Start --> */}
+    <section class="home-section-2 home-section-bg pt-0 overflow-hidden">
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-12">
+                    <div class="slider-animate">
+                        <div>
+                            <div class="home-contain rounded-0 p-0">
+                                <img src="https://img6.hkrtcdn.com/41982/bnr_4198105_o.jpg"
+                                    class="img-fluid bg-img blur-up lazyload" alt="" style={{width:'100%'}} />
+                                <div class="home-detail home-big-space p-center-left home-overlay ">
+                                    <div class="container-fluid-lg">
+                                        <div>
+                                            <h6 class="ls-expanded theme-color text-uppercase">Weekend Special offer
+                                            </h6>
+                                            <h1 class="heding-2">Premium Quality Dry Fruits</h1>
+                                            <h2 class="content-2">Dryfruits shopping made Easy</h2>
+                                            <h5 class="text-content">Fresh & Top Quality Dry Fruits are available here!
+                                            </h5>
+                                            <button
+                                                class="btn theme-bg-color btn-md text-white fw-bold mt-md-4 mt-2 mend-auto"
+                                                onclick="location.href = 'shop-left-sidebar.html';">Shop Now <i
+                                                    class="fa-solid fa-arrow-right icon"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {/* <!-- Home Section End --> */}
     </>
   )
 }
